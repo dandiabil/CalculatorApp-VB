20827dc8fc5011b3de688d705677f34fd6d251ea
Dandi Sabilli Izzulhaq <45232854+dandiabil@users.noreply.github.com>
2022-04-03 14:09:15 +0700

    initial commit

---
+++ KalkulatorApp/Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tbDisplay = New System.Windows.Forms.TextBox()
+        Me.btnSeven = New System.Windows.Forms.Button()
+        Me.btnEight = New System.Windows.Forms.Button()
+        Me.btnNine = New System.Windows.Forms.Button()
+        Me.btnSum = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnSix = New System.Windows.Forms.Button()
+        Me.btnMin = New System.Windows.Forms.Button()
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.btnThree = New System.Windows.Forms.Button()
+        Me.btnZero = New System.Windows.Forms.Button()
+        Me.btnTimes = New System.Windows.Forms.Button()
+        Me.btnEqual = New System.Windows.Forms.Button()
+        Me.btnDivision = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'tbDisplay
+        '
+        Me.tbDisplay.Cursor = System.Windows.Forms.Cursors.Default
+        Me.tbDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tbDisplay.Location = New System.Drawing.Point(81, 50)
+        Me.tbDisplay.Multiline = True
+        Me.tbDisplay.Name = "tbDisplay"
+        Me.tbDisplay.ReadOnly = True
+        Me.tbDisplay.Size = New System.Drawing.Size(418, 148)
+        Me.tbDisplay.TabIndex = 0
+        Me.tbDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnSeven
+        '
+        Me.btnSeven.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSeven.Location = New System.Drawing.Point(81, 213)
+        Me.btnSeven.Name = "btnSeven"
+        Me.btnSeven.Size = New System.Drawing.Size(100, 85)
+        Me.btnSeven.TabIndex = 1
+        Me.btnSeven.Text = "7"
+        Me.btnSeven.UseVisualStyleBackColor = True
+        '
+        'btnEight
+        '
+        Me.btnEight.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEight.Location = New System.Drawing.Point(187, 213)
+        Me.btnEight.Name = "btnEight"
+        Me.btnEight.Size = New System.Drawing.Size(100, 85)
+        Me.btnEight.TabIndex = 1
+        Me.btnEight.Text = "8"
+        Me.btnEight.UseVisualStyleBackColor = True
+        '
+        'btnNine
+        '
+        Me.btnNine.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNine.Location = New System.Drawing.Point(293, 213)
+        Me.btnNine.Name = "btnNine"
+        Me.btnNine.Size = New System.Drawing.Size(100, 85)
+        Me.btnNine.TabIndex = 1
+        Me.btnNine.Text = "9"
+        Me.btnNine.UseVisualStyleBackColor = True
+        '
+        'btnSum
+        '
+        Me.btnSum.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSum.Location = New System.Drawing.Point(399, 213)
+        Me.btnSum.Name = "btnSum"
+        Me.btnSum.Size = New System.Drawing.Size(100, 85)
+        Me.btnSum.TabIndex = 1
+        Me.btnSum.Text = "+"
+        Me.btnSum.UseVisualStyleBackColor = True
+        '
+        'btnFour
+        '
+        Me.btnFour.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFour.Location = New System.Drawing.Point(81, 304)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(100, 85)
+        Me.btnFour.TabIndex = 1
+        Me.btnFour.Text = "4"
+        Me.btnFour.UseVisualStyleBackColor = True
+        '
+        'btnFive
+        '
+        Me.btnFive.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnFive.Location = New System.Drawing.Point(187, 304)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(100, 85)
+        Me.btnFive.TabIndex = 1
+        Me.btnFive.Text = "5"
+        Me.btnFive.UseVisualStyleBackColor = True
+        '
+        'btnSix
+        '
+        Me.btnSix.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSix.Location = New System.Drawing.Point(293, 304)
+        Me.btnSix.Name = "btnSix"
+        Me.btnSix.Size = New System.Drawing.Size(100, 85)
+        Me.btnSix.TabIndex = 1
+        Me.btnSix.Text = "6"
+        Me.btnSix.UseVisualStyleBackColor = True
+        '
+        'btnMin
+        '
+        Me.btnMin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnMin.Location = New System.Drawing.Point(399, 304)
+        Me.btnMin.Name = "btnMin"
+        Me.btnMin.Size = New System.Drawing.Size(100, 85)
+        Me.btnMin.TabIndex = 1
+        Me.btnMin.Text = "-"
+        Me.btnMin.UseVisualStyleBackColor = True
+        '
+        'btnOne
+        '
+        Me.btnOne.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnOne.Location = New System.Drawing.Point(81, 395)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(100, 85)
+        Me.btnOne.TabIndex = 1
+        Me.btnOne.Text = "1"
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTwo.Location = New System.Drawing.Point(187, 395)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(100, 85)
+        Me.btnTwo.TabIndex = 1
+        Me.btnTwo.Text = "2"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'btnThree
+        '
+        Me.btnThree.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnThree.Location = New System.Drawing.Point(293, 395)
+        Me.btnThree.Name = "btnThree"
+        Me.btnThree.Size = New System.Drawing.Size(100, 85)
+        Me.btnThree.TabIndex = 1
+        Me.btnThree.Text = "3"
+        Me.btnThree.UseVisualStyleBackColor = True
+        '
+        'btnZero
+        '
+        Me.btnZero.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnZero.Location = New System.Drawing.Point(187, 486)
+        Me.btnZero.Name = "btnZero"
+        Me.btnZero.Size = New System.Drawing.Size(100, 85)
+        Me.btnZero.TabIndex = 1
+        Me.btnZero.Text = "0"
+        Me.btnZero.UseVisualStyleBackColor = True
+        '
+        'btnTimes
+        '
+        Me.btnTimes.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnTimes.Location = New System.Drawing.Point(399, 395)
+        Me.btnTimes.Name = "btnTimes"
+        Me.btnTimes.Size = New System.Drawing.Size(100, 85)
+        Me.btnTimes.TabIndex = 1
+        Me.btnTimes.Text = "x"
+        Me.btnTimes.UseVisualStyleBackColor = True
+        '
+        'btnEqual
+        '
+        Me.btnEqual.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEqual.Location = New System.Drawing.Point(293, 486)
+        Me.btnEqual.Name = "btnEqual"
+        Me.btnEqual.Size = New System.Drawing.Size(100, 85)
+        Me.btnEqual.TabIndex = 1
+        Me.btnEqual.Text = "="
+        Me.btnEqual.UseVisualStyleBackColor = True
+        '
+        'btnDivision
+        '
+        Me.btnDivision.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDivision.Location = New System.Drawing.Point(399, 486)
+        Me.btnDivision.Name = "btnDivision"
+        Me.btnDivision.Size = New System.Drawing.Size(100, 85)
+        Me.btnDivision.TabIndex = 1
+        Me.btnDivision.Text = "/"
+        Me.btnDivision.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(81, 486)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(100, 85)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "C"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(592, 618)
+        Me.Controls.Add(Me.btnEqual)
+        Me.Controls.Add(Me.btnTimes)
+        Me.Controls.Add(Me.btnMin)
+        Me.Controls.Add(Me.btnSum)
+        Me.Controls.Add(Me.btnThree)
+        Me.Controls.Add(Me.btnSix)
+        Me.Controls.Add(Me.btnDivision)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnZero)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnFive)
+        Me.Controls.Add(Me.btnOne)
+        Me.Controls.Add(Me.btnNine)
+        Me.Controls.Add(Me.btnFour)
+        Me.Controls.Add(Me.btnEight)
+        Me.Controls.Add(Me.btnSeven)
+        Me.Controls.Add(Me.tbDisplay)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbDisplay As TextBox
+    Friend WithEvents btnSeven As Button
+    Friend WithEvents btnEight As Button
+    Friend WithEvents btnNine As Button
+    Friend WithEvents btnSum As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnFive As Button
+    Friend WithEvents btnSix As Button
+    Friend WithEvents btnMin As Button
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents btnThree As Button
+    Friend WithEvents btnZero As Button
+    Friend WithEvents btnTimes As Button
+    Friend WithEvents btnEqual As Button
+    Friend WithEvents btnDivision As Button
+    Friend WithEvents btnClear As Button
+End Class
---
+++ KalkulatorApp/Form1.vb
@@ -0,0 +1,229 @@
+﻿Public Class Form1
+    Dim operasiHitung As String
+    Dim firstNumber, secondNumber As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tbDisplay.Text = "0"
+    End Sub
+
+    Private Sub btnZero_Click(sender As Object, e As EventArgs) Handles btnZero.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "0"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "0"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "0"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) Handles btnOne.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "1"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "1"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "1"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) Handles btnTwo.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "2"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "2"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "2"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnThree_Click(sender As Object, e As EventArgs) Handles btnThree.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "3"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "3"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "3"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnFour_Click(sender As Object, e As EventArgs) Handles btnFour.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "4"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "4"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "4"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnFive_Click(sender As Object, e As EventArgs) Handles btnFive.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "5"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "5"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "5"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnSix_Click(sender As Object, e As EventArgs) Handles btnSix.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "6"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "6"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "6"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnSeven_Click(sender As Object, e As EventArgs) Handles btnSeven.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "7"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "7"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "7"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnEight_Click(sender As Object, e As EventArgs) Handles btnEight.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "8"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "8"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "8"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnNine_Click(sender As Object, e As EventArgs) Handles btnNine.Click
+
+        If tbDisplay.Text = "0" Then
+            tbDisplay.Text = "9"
+            firstNumber = tbDisplay.Text
+        Else
+            If operasiHitung = Nothing Then
+                tbDisplay.Text = tbDisplay.Text & "9"
+                firstNumber = tbDisplay.Text & firstNumber
+            Else
+                tbDisplay.Text = secondNumber & "9"
+                secondNumber = tbDisplay.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnEqual_Click(sender As Object, e As EventArgs) Handles btnEqual.Click
+        MsgBox("First Number : " & firstNumber & vbCrLf & "Operator : " & operasiHitung & vbCrLf _
+            & "Second Number : " & secondNumber)
+        Dim hasil As Integer = 0
+
+        If operasiHitung = "+" Then
+            hasil = Int(firstNumber) + Int(secondNumber)
+        ElseIf operasiHitung = "-" Then
+            hasil = Int(firstNumber) - Int(secondNumber)
+        ElseIf operasiHitung = "x" Then
+            hasil = Int(firstNumber) * Int(secondNumber)
+        ElseIf operasiHitung = "/" Then
+            hasil = Int(firstNumber) / Int(secondNumber)
+        End If
+
+        tbDisplay.Text = hasil
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        tbDisplay.Text = "0"
+        firstNumber = Nothing
+        secondNumber = Nothing
+        operasiHitung = Nothing
+    End Sub
+
+    Private Sub btnSum_Click(sender As Object, e As EventArgs) Handles btnSum.Click
+        If operasiHitung = "-" Or operasiHitung = "x" Or operasiHitung = "/" Then
+            MsgBox("Operator Sudah Dipilih")
+        Else
+            tbDisplay.Text = "+"
+            operasiHitung = "+"
+        End If
+    End Sub
+
+    Private Sub btnMin_Click(sender As Object, e As EventArgs) Handles btnMin.Click
+        If operasiHitung = "+" Or operasiHitung = "x" Or operasiHitung = "/" Then
+            MsgBox("Operator Sudah Dipilih")
+        Else
+            tbDisplay.Text = "-"
+            operasiHitung = "-"
+        End If
+    End Sub
+
+    Private Sub btnTimes_Click(sender As Object, e As EventArgs) Handles btnTimes.Click
+        If operasiHitung = "-" Or operasiHitung = "+" Or operasiHitung = "/" Then
+            MsgBox("Operator Sudah Dipilih")
+        Else
+            tbDisplay.Text = "x"
+            operasiHitung = "x"
+        End If
+    End Sub
+
+    Private Sub btnDivision_Click(sender As Object, e As EventArgs) Handles btnDivision.Click
+        If operasiHitung = "-" Or operasiHitung = "x" Or operasiHitung = "/" Then
+            MsgBox("Operator Sudah Dipilih")
+        Else
+            tbDisplay.Text = "/"
+            operasiHitung = "/"
+        End If
+    End Sub
+End Class
